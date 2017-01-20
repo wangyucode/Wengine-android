de161217a293ca2fd9ab6d2645a5998dd1c68ea5
王郁 <www_wayne_001@163.com>
2017-01-20 17:34:27 +0800

    Update AirplaneDemo.java

--- demo/src/main/java/cn/wycode/game/demo/AirplaneDemo.java
+++ demo/src/main/java/cn/wycode/game/demo/AirplaneDemo.java
@@ -1,14 +1,17 @@
 package cn.wycode.game.demo;
 
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Rect;
+import android.support.v7.app.AlertDialog;
 import android.view.MotionEvent;
 
 import cn.wycode.wengine.Wengine;
 import cn.wycode.wengine.animation.MoveAnimation;
 import cn.wycode.wengine.sprite.BaseSprite;
+import cn.wycode.wengine.sprite.TextSprite;
 import cn.wycode.wengine.utils.ScreenInfo;
 import cn.wycode.wengine.utils.Timer;
 
@@ -21,20 +24,28 @@ public class AirplaneDemo extends Wengine {
 
     private BaseSprite airplane;
 
-    private Bitmap bm_space, bg_2x, bm_airplane;
+    private Bitmap bm_space, bm_2x, bm_airplane, bm_enemy;
 
     private Rect draw_bg_rect;
     private int bg_top;
-    private Timer bg_timer, bulletTimer;
+    private Timer bg_timer, bulletTimer, enemyTimer;
     private int bg_seed = 100;
 
     private int planeSize = 100;
 
-    private int firePerSecond = 30;
+    private int firePerSecond = 6;
+    private int enemyPerSecond = 3;
+    private int enemySpeed = 200;
+
+    private int enemyW;
+    private int enemyH;
 
     private int bulletW = 4;
     private int bulletH = 8;
     private int bulletSpeed = 500;
+
+    private TextSprite scoreSprite;
+    private int score;
 
     @Override
     protected void init() {
@@ -52,8 +63,8 @@ public class AirplaneDemo extends Wengine {
 
         bg_timer = new Timer();
 
-        bg_2x = Bitmap.createBitmap(ScreenInfo.width, ScreenInfo.height * 4, Bitmap.Config.ARGB_8888);
-        Canvas c_bg = new Canvas(bg_2x);
+        bm_2x = Bitmap.createBitmap(ScreenInfo.width, ScreenInfo.height * 4, Bitmap.Config.ARGB_8888);
+        Canvas c_bg = new Canvas(bm_2x);
         Rect dst = new Rect(0, 0, ScreenInfo.width, ScreenInfo.height * 2);
         c_bg.drawBitmap(bm_space, null, dst, null);
         dst.set(0, ScreenInfo.height * 2, ScreenInfo.width, ScreenInfo.height * 4);
@@ -63,14 +74,22 @@ public class AirplaneDemo extends Wengine {
         airplane.convertToDpSize();
         airplane.setName("airplane");
 
+        bm_enemy = texture.Rotate(bm_airplane, 180);
+
         addSprite(airplane);
 
         bulletTimer = new Timer();
+        enemyTimer = new Timer();
+
+        enemyW = enemyH = planeSize / 2;
+
+        scoreSprite = new TextSprite("", ScreenInfo.width - ScreenInfo.dp2px(200), 20, 20, Color.WHITE);
+        addSprite(scoreSprite);
     }
 
     @Override
     public void draw(Canvas canvas) {
-        canvas.drawBitmap(bg_2x, draw_bg_rect, ScreenInfo.screenRect, null);
+        canvas.drawBitmap(bm_2x, draw_bg_rect, ScreenInfo.screenRect, null);
     }
 
     @Override
@@ -84,7 +103,43 @@ public class AirplaneDemo extends Wengine {
         draw_bg_rect.set(0, bg_top, ScreenInfo.width, ScreenInfo.height + bg_top);
 
         //生成子弹
-        if (bulletTimer.getElapse() > 1000f / firePerSecond) {
+        generateBullet();
+        //生成敌人
+        generateEnemy();
+
+        scoreSprite.setText("Score:" + score);
+    }
+
+    private void generateEnemy() {
+        if (enemyTimer.getElapseNotReset() > 1000f / enemyPerSecond) {
+            MoveAnimation animation = new MoveAnimation(90, enemySpeed);
+            BaseSprite enemy = null;
+            float enemyX = random.nextFloat() * (ScreenInfo.width - ScreenInfo.dp2px(enemyW));
+            float enemyY = 0 - ScreenInfo.dp2px(enemyH) + 2;
+            for (BaseSprite s : spriteRecyclePool) {
+                if ("enemy".equals(s.getName())) {
+                    s.setAlive(true);
+                    s.removeAllAnimation();
+                    s.setX(enemyX);
+                    s.setY(enemyY);
+                    enemy = s;
+                    spriteRecyclePool.remove(s);
+                }
+            }
+
+            if (enemy == null) {
+                enemy = new BaseSprite(bm_enemy, enemyX, enemyY, enemyW, enemyH);
+                enemy.convertToDpSize();
+                enemy.setName("enemy");
+            }
+            enemy.addAnimation(animation);
+            addSprite(enemy);
+            enemyTimer.reset();
+        }
+    }
+
+    private void generateBullet() {
+        if (bulletTimer.getElapseNotReset() > 1000f / firePerSecond) {
             MoveAnimation animation = new MoveAnimation(-90, bulletSpeed);
             BaseSprite bullet = null;
             float bulletX = airplane.getCenterX() - ScreenInfo.dp2px(bulletW) / 2f;
@@ -102,22 +157,70 @@ public class AirplaneDemo extends Wengine {
 
             if (bullet == null) {
                 bullet = new BaseSprite(bulletX, bulletY, bulletW, bulletH);
+                bullet.setBackgroundColor(Color.YELLOW);
                 bullet.convertToDpSize();
                 bullet.setName("bullet");
             }
             bullet.addAnimation(animation);
             addSprite(bullet);
+            bulletTimer.reset();
         }
     }
 
     @Override
     public void collided(BaseSprite a, BaseSprite b) {
-
+        if ("enemy".equals(a.getName())) {
+            if ("bullet".equals(b.getName())) {
+                a.setAlive(false);
+                score += 100;
+            } else if ("airplane".equals(b.getName())) {
+                gameOver();
+            }
+        }
+        if ("enemy".equals(b.getName())) {
+            if ("bullet".equals(a.getName())) {
+                b.setAlive(false);
+                score += 100;
+            } else if ("airplane".equals(a.getName())) {
+                gameOver();
+            }
+        }
     }
 
     @Override
     public void touch(MotionEvent event) {
         airplane.setX(event.getX() - airplane.getW() / 2);
         airplane.setY(event.getY() - airplane.getH() / 2);
+    }
+
+    @Override
+    public void release() {
+        if (bm_space != null)
+            bm_space.recycle();
+        if (bm_airplane != null)
+            bm_airplane.recycle();
+        if (bm_2x != null)
+            bm_2x.recycle();
+        if (bm_enemy != null)
+            bm_enemy.recycle();
+    }
+
+    private void gameOver() {
+        pause();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                AlertDialog dialog = new AlertDialog.Builder(mContext)
+                        .setMessage("Game Over!")
+                        .setCancelable(false)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        }).create();
+                dialog.show();
+            }
+        });
     }
 }
